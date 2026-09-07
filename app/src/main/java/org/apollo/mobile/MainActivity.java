d1986bb27029565e3119fb628cd6c56f4f7236d7
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-09-07 14:38:07 -0300

    refactor: reorganize splash-to-welcome screen workflow

--- app/src/main/java/org/apollo/mobile/MainActivity.java
+++ app/src/main/java/org/apollo/mobile/MainActivity.java
@@ -1,6 +1,9 @@
 package org.apollo.mobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +11,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.apollo.mobile.view.auth.WelcomeActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DURATION_MS = 1200L;
+
+    private final Handler handler = new Handler(Looper.getMainLooper());
+
+    private final Runnable openWelcomeScreen = () -> {
+        Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+        startActivity(intent);
+        finish();
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        // Alterado de activity_main para activity_splash para resolver o erro de compilação
         setContentView(R.layout.activity_splash);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        handler.postDelayed(openWelcomeScreen, SPLASH_DURATION_MS);
+    }
+
+    @Override
+    protected void onDestroy() {
+        handler.removeCallbacks(openWelcomeScreen);
+        super.onDestroy();
     }
 }
